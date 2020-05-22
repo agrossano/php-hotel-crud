0e4f7034490c44177baecbd37e938ae33a092d3d
Alessandro <grossano.alessandro@gmail.com>
2020-05-22 16:30:07 +0200

    Terminato

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-console.log('cia')
-
-
 $.ajax({
   url: "getPagamenti.php",
   method: 'GET',
   success: function (data) {
+    data.forEach(element => {
+      $("ul").append("<li>" + element + "</li>");
+    });
+
   },
   error: function (err) {
     console.log(err)
   }
 
 });
-
-
 
 
 
